44897e70ae577b4537cdf59d66820951f881ca9b
gustavofurtado7 <gustavofurtadopeg@gmail.com>
2026-08-21 18:28:03 -0300

    Implementacao do javaScript

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const convertButton = document.querySelector(".convert-button")
+
+function convertValues(){
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+
+    const dolarToday = 5.14
+
+    const convertedValue = inputCurrencyValue / dolarToday
+    console.log(convertedValue)
+}
+
+convertButton.addEventListener("click", convertValues)
